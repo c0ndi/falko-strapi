da08acbeba54170d459310ca8927873ae1c703a4
Konrad <kowalik0konrad@gmail.com>
2023-04-12 19:01:12 +0200

    Custom messages single type

---
+++ src/api/customer-message/controllers/customer-message.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * customer-message controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::customer-message.customer-message');
---
+++ src/api/customer-message/routes/customer-message.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * customer-message router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::customer-message.customer-message');
